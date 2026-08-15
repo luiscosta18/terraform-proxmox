b49a69935e100935e975acf0e47785b9176b83f8
Luís Costa <46764447+luiscosta18@users.noreply.github.com>
2026-08-15 17:48:34 +0100

    Update main.tf

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,7 +1,10 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">=1.15"
   required_providers {
-    proxmox = { source = "bpg/proxmox" }
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = ">=0.111"
+    }
   }
 }
 
